8daded621c5d9d57d90b3686b418e540d49171c2
SailorMercury <mizunoyukino@hotmail.com>
2011-10-14 23:44:40 +0800

    fixed routing again

--- app/controllers/answer_session_controller.rb
+++ app/controllers/answer_session_controller.rb
@@ -1,7 +1,7 @@
 class AnswerSessionController < ApplicationController
   def new
     @card_user = User.find(params[:user]) #owner of card
-    #redirect_to root_path if !@user
+    redirect_to root_path if @user
     @hash_card = @card_user.hashcards.find_by_name(params[:hash_card]) #hash card that is pass to the url
     redirect_to root_path if !@hash_card
     
