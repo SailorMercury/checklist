13bdfa930cadd57c1bf2713ec2f1b0337db5ce0f
SailorMercury <mizunoyukino@hotmail.com>
2011-10-22 16:03:49 +0800

    design improment

--- app/controllers/answer_session_controller.rb
+++ app/controllers/answer_session_controller.rb
@@ -1,14 +1,21 @@
 class AnswerSessionController < ApplicationController
   def new
     @card_user = User.find(params[:user]) #owner of card
-    redirect_to root_path if @user
+    redirect_to root_path if !@card_user
     @hash_card = @card_user.hashcards.find_by_name(params[:hash_card]) #hash card that is pass to the url
     redirect_to root_path if !@hash_card
     
     @hash_card.tasks.each do |t|
-      next if !Answer.where(:task_id=>t.id,:user_id=>current_user.id).empty?
-       t.answers.build :column_name => "default", :user_id => current_user.id
+       if current_user.nil? || !Answer.where(:task_id=>t.id,:user_id=>current_user.id).empty?
+          t.answers.each do |a|
+            a.value = nil
+          end
+       else
+          t.answers.build :column_name => "default", :user_id => current_user.id
+       end
     end
+    
+    
   end
   
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :task
+  
+  scope :for_user, lambda { |user| {:conditions => {user_id: user.id}}}
 end
